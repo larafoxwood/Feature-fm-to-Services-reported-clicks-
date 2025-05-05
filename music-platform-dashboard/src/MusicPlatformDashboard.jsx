13ed7655262d382d6eb09813779ff1bb7134d5f1
larafoxwood <larafoxwood@gmail.com>
2025-05-05 14:20:56 -0700

    Add files via upload

---
+++ music-platform-dashboard/src/MusicPlatformDashboard.jsx
@@ -0,0 +1,8 @@
+// Replace this with the full MusicPlatformDashboard component code you saved
+import React from 'react';
+
+const MusicPlatformDashboard = () => {
+  return <div>Music Platform Dashboard Component</div>;
+};
+
+export default MusicPlatformDashboard;
